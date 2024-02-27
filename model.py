e2393a88ce898bb317c5ade8c58cd94a997260a7
Hushdon Tohovaka <htt0040@arastudent.ac.nz>
2024-02-27 23:14:53 +1300

    Added tkinter inheritance

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+# The model isnt aware of the controller or the view
 class Model:
     
     def __init__(self):
--- view.py
+++ view.py
@@ -1,7 +1,14 @@
-class View:
+# These are imported and are packaged from tkinter #@7.15mins
+import tkinter as tk
+from tkinter import ttk
+
+class View(tk.Tk):
     
     def __init__(self,controller):
         self.controller = controller
+
+        # This essentially helps us with inheritance from tkinter.
+        super().__init__()
 
     def main(self):
         print("In main of view")
