5f24729f68d7926ed4487ce018c366daba9fd0c0
Hushdon Tohovaka <htt0040@arastudent.ac.nz>
2024-02-27 23:10:41 +1300

    Connected Controller, View and Model

--- controller.py
+++ controller.py
@@ -1,11 +1,26 @@
-class Controller:
-    
-    #Added the 
+from model import Model
+from view import View
 
+# In MVC the controller knows about the model and the view
+# Now we have access to both those classes
+
+# The model isnt aware of the controller or the view
+# But the view knows about the controller
+
+class Controller:
     def __init__(self):
-        pass
+        self.model = Model()
+        self.view = View(self)
     
+    def main(self):
+        self.view.main()
 
 # The entry point to the program 
 if __name__ == '__main__':
-    print("Hello World")
+    # print("Hello World")
+    
+    # Created an instance of the controller
+    calculator = Controller()
+
+    # Called the main function on the controller
+    calculator.main()
--- view.py
+++ view.py
@@ -1,4 +1,7 @@
 class View:
     
-    def __init__(self):
-        pass
+    def __init__(self,controller):
+        self.controller = controller
+
+    def main(self):
+        print("In main of view")
